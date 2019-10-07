ef233b9e7ec42dcaa7ff6d029d26e73773502860
centeno-a <55461796+centeno-a@users.noreply.github.com>
2019-10-07 15:14:33 -0500

    Add files via upload

---
+++ script02.py
@@ -0,0 +1,11 @@
+# el comando print imprime un mensaje en la pantalla o dispositivo de salida. El mensaje puede ser una cadena de caracteres o cualquier objecto que sea convertible a cadena de caracteres
+
+#el comando input permite al usuario introducir informacion utilizando el teclado la variable donde se guarda dicha informacion es de tipo string o cadena de caracteres 
+#el comando int convierte cualquier dato en tipo entero.
+#el comando float convierte cualquier dato en tipo decimal.
+nombre = input('inserta tu nombre: ')
+edad = int(input('inserta tu edad: '))
+estatura = float(input('inseta tu estatura:'))
+print('naci en',1990-edad, 'y mido',estatura)
+print('Hola a todos')
+
