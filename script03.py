8aedd8b71bdac9832fdf6290defccb17f7759199
centeno-a <55461796+centeno-a@users.noreply.github.com>
2019-10-08 18:52:15 -0500

    Add files via upload

---
+++ script03.py
@@ -0,0 +1,17 @@
+# primer programa
+
+print ('luis alberto centeno')
+print ('cerrada de san pablo 8 ')
+print ('el cadete de linares')
+print (' naucalpan forever ')
+print ('cp 52770')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('                                                                 diego jacinto deciga')
+print ('                                                              barrio buena vista sin #')
+print ('                                                              naucalpan de juarez')
+
